d0d8292cac677326fb7dee650156a0e687475d3d
Matt Swensen <mjs@marriottschool.byu.edu>
2012-08-20 14:13:58 -0600

    Starting the Models and Collections

--- Day1/backbone-example.js
+++ Day1/backbone-example.js
@@ -1,1 +1,25 @@
 /* Calendar code goes here */
+
+/* Models */
+
+var CalendarItem = Backbone.Model.extend();
+
+var Category = Backbone.Model.extend();
+
+var Heading = Backbone.Model.extend();
+
+/* Collections */
+
+var CalendarItems = Backbone.Collection.extend({
+  model: CalendarItem
+});
+
+var Categories = Backbone.Collection.extend({
+  model: Category
+});
+
+var Headings = Backbone.Collection.extend({
+  model: Heading
+});
+
+/* Views */
