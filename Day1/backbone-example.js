e1ac41799728a0aeee00b10541ef3fe1486b0b87
unknown <jgallred@CTL-53236.byu.local>
2012-08-20 15:31:43 -0600

    Resolving my last failed merge.

--- Day1/backbone-example.js
+++ Day1/backbone-example.js
@@ -25,13 +25,47 @@ var Headings = Backbone.Collection.extend({
 
 /* Views */
 
-var CalendarView = Backbone.View.extend({});
+var CalendarView = Backbone.View.extend({
+	el:'#calendarTable',
+	initialize:function(params) {
+		// Just redraw all the date rows, in case calendar items move
+		params.categories.on('change:headingId', this.redrawDates, this);
+		this.categories = params.categories;
+		this.headings = params.headings;
+		this.calendarItems = params.calendarItems;
+	},
+	render:function() {
+		var headingsView = new HeadingsRow({collection:this.headings, categories:this.categories});
+		this.$el.append(headingsView.render().$el);		
+		this.redrawDates();
+		return this;
+	},
+	redrawDates: function() {
+		this.$('tbody').remove(); // Remove old view
+		
+		this.$el.append('<tbody>');
+		var today = Date.parse("8/1/2012");
+		for(var i = 0; i < 30; i++) {
+			today = today.addDays(1);
+			var dateView = new DateRow({date:today, headings:this.headings, calendarItems:this.calendarItems});
+			this.$el.append(dateView.render().$el);
+		}
+	}
+});
 
-var HeadingsRow = Backbone.View.extend({});
+var HeadingsRow = Backbone.View.extend({
+	tagName:'thead',
+	render:function() {
+		this.$el.append('<tr>');
+		return this;
+	}
+});
 
 var HeadingCell = Backbone.View.extend({});
 
-var DatesRow = Backbone.View.extend({});
+var DateRow = Backbone.View.extend({
+	tagName:'tr'
+});
 
 var DateCell = Backbone.View.extend({});
 
@@ -92,3 +126,7 @@ calendarItemsCollection.fetch({
   async: false// halts execution until response is received.
 });
 console.log(calendarItemsCollection);
+
+
+
+(new CalendarView({headings:headingsCollection, calendarItems:calendarItemsCollection, categories:categoriesCollection})).render();
