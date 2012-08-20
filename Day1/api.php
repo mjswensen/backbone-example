866c28545b987471d1df83496bea3c3b7d11ebeb
Matt Swensen <mjs@marriottschool.byu.edu>
2012-08-20 15:01:42 -0600

    Initializing collections

--- Day1/api.php
+++ Day1/api.php
@@ -1,20 +1,20 @@
 <?php
 
 $data = array(
-  array('date' => '', 'text' => 'Homework 1', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Homework 2', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Homework 3', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Homework 4', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Homework 5', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Midterm Review', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Reading Assignment', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Midterm Evaluation', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Homework 6', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Homework 7', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Homework 8', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Homework 9', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Homework 10', 'categoryId' => ''),
-  array('date' => '', 'text' => 'Final Exam Review', 'categoryId' => '')
+  array('date' => '', 'text' => 'Homework 1', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Homework 2', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Homework 3', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Homework 4', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Homework 5', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Midterm Review', 'categoryId' => 'cat3'),
+  array('date' => '', 'text' => 'Reading Assignment', 'categoryId' => 'cat2'),
+  array('date' => '', 'text' => 'Midterm Evaluation', 'categoryId' => 'cat4'),
+  array('date' => '', 'text' => 'Homework 6', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Homework 7', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Homework 8', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Homework 9', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Homework 10', 'categoryId' => 'cat1'),
+  array('date' => '', 'text' => 'Final Exam Review', 'categoryId' => 'cat3')
 );
 
 header("Content-Type: application/json");
--- Day1/backbone-example.js
+++ Day1/backbone-example.js
@@ -34,3 +34,61 @@ var HeadingCell = Backbone.View.extend({});
 var DatesRow = Backbone.View.extend({});
 
 var DateCell = Backbone.View.extend({});
+
+/* App */
+
+// Create a new headings collection, with data.
+var headingsCollection = new Headings([
+  {
+    id: 'col2',
+    title: 'Column 2',
+    order: 2
+  },
+  {
+    id: 'col1',
+    title: 'Column 1',
+    order: 1
+  },
+  {
+    id: 'col3',
+    title: 'Column 3',
+    order: 3
+  }
+]);
+console.log(headingsCollection);
+
+// Create a new categories collection, with data.
+var categoriesCollection = new Categories([
+  {
+    id: 'cat1',
+    headingId: 'col2',
+    title: 'Homework Assignments',
+    order: 1
+  },
+  {
+    id: 'cat2',
+    headingId: 'col2',
+    title: 'Reading Assignments',
+    order: 1
+  },
+  {
+    id: 'cat3',
+    headingId: 'col1',
+    title: 'Exams',
+    order: 1
+  },
+  {
+    id: 'cat4',
+    headingId: 'col3',
+    title: 'Other',
+    order: 1
+  }
+]);
+console.log(categoriesCollection);
+
+// Create a new calendar items collection, and fetch its data from the services.
+var calendarItemsCollection = new CalendarItems();
+calendarItemsCollection.fetch({
+  async: false// halts execution until response is received.
+});
+console.log(calendarItemsCollection);
