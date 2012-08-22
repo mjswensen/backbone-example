33f540a6862fd6260c2b26731cd7cc68a0a06c4b
jgallred <jgallred86@gmail.com>
2012-08-22 14:05:34 -0600

    Added more explanatory comments.  Improved design a bit. Fixed event bug where you could only add 1 item to each row.

--- Day1/backbone-example.js
+++ Day1/backbone-example.js
@@ -1,5 +1,3 @@
-/* Calendar code goes here */
-
 /* Models */
 
 var CalendarItem = Backbone.Model.extend();
@@ -17,21 +15,33 @@ var CalendarItems = Backbone.Collection.extend({
 
 var Categories = Backbone.Collection.extend({
   model: Category,
-  comparator:function(model){return model.get('order');}
+  comparator:function(model1, model2){
+	// A 'sort' comparator
+	if(model1.get('order') === model2.get('order')) {
+		return 0;
+	} else {
+		return (model1.get('order') < model2.get('order')) ? -1 : 1;
+	}
+  }
 });
 
 var Headings = Backbone.Collection.extend({
   model: Heading,
-  comparator:function(model){return model.get('order');}
+  comparator:function(model){
+	// A 'sortBy' comparator
+	return model.get('order');
+  }
 });
 
 /* Views */
 
 var CalendarView = Backbone.View.extend({
-	el:'#calendarTable',
+	el:'#calendarTable', // Use a jQuery selector in el to attach view to existing DOM element
 	initialize:function(params) {
 		// Just redraw all the date rows, in case calendar items move
 		params.categories.on('change:headingId', this.redrawDates, this);
+		
+		// For this example, I've chosen to abstract out global data by passing the global collections as parameters
 		this.categories = params.categories;
 		this.headings = params.headings;
 		this.calendarItems = params.calendarItems;
@@ -39,9 +49,9 @@ var CalendarView = Backbone.View.extend({
 	render:function() {
 		this.$el.append('<thead id="calendarHead">');
 		var headingsView = new HeadingsRow({collection:this.headings, categories:this.categories});
-		this.$('thead').append(headingsView.render().$el);		
+		this.$('thead').append(headingsView.render().$el);	// this.$() is equivalent to this.$el.find()	
 		this.redrawDates();
-		return this;
+		return this; // Return "this" for method chaining, as seen above in "headingsView.render().$el"
 	},
 	redrawDates: function() {
 		this.$('tbody').remove(); // Remove old view
@@ -56,18 +66,16 @@ var CalendarView = Backbone.View.extend({
 });
 
 var HeadingsRow = Backbone.View.extend({
-	tagName:'tr',
+	tagName:'tr', // TagName property determines type of element view represents. Default is div
 	initialize:function(params){
 		this.categories = params.categories;
 	},
 	render:function() {
-		this.$el.append('<th id="dateColumn">Date</th>');
+		this.$el.append('<th id="dateColumn">Date</th>'); // this.$el a jQuery instance of the views DOM element, equivalent to $(this.el)
 		var view = this;
-		var headerNum = 0; // Allows checkboxes to have unique ids for labels' "for" attribute
 		this.collection.forEach(function(headingModel){
-			var headingView = new HeadingCell({model:headingModel, categories:view.categories, instanceNum:headerNum});
+			var headingView = new HeadingCell({model:headingModel, categories:view.categories});
 			view.$el.append(headingView.render().$el);
-			headerNum++;
 		});
 		return this;
 	}
@@ -75,8 +83,8 @@ var HeadingsRow = Backbone.View.extend({
 
 var HeadingCell = Backbone.View.extend({
 	tagName:'th',
-	id:'uid_',
-	className:"calendarColumnHeader",	
+	id:'uid_', // This id will be a part of this.el when it is initialized
+	className:"calendarColumnHeader", // This string of css classes will be a part of this.el when it is initialized
 	template: Handlebars.compile(
 		'<div class="innerWrapper" style="width: 100%; position: relative; ">'
 			+'<div class="titleWrapper">'
@@ -87,8 +95,8 @@ var HeadingCell = Backbone.View.extend({
 				+'<label>Show Categories:</label>'  
 				+'{{#each categories}}'
 				+'<div class="inlineLabel ">' 
-					+'<input type="checkbox" id="{{num}}uid_{{id}}" name="{{num}}uid_{{id}}">'        
-					+'<label id="{{num}}uid_{{id}}_label" for="{{num}}uid_{{id}}" >{{title}}</label>'
+					+'<input type="checkbox" id="{{heading_order}}uid_{{id}}" name="{{heading_order}}uid_{{id}}">'        
+					+'<label id="{{heading_order}}uid_{{id}}_label" for="{{heading_order}}uid_{{id}}" >{{title}}</label>'
 				+'</div>'    
 				+'{{/each}}'			
 			+'</div>'
@@ -96,8 +104,7 @@ var HeadingCell = Backbone.View.extend({
 		+'</div>'),
 	initialize:function(params){
 		this.categories = params.categories;
-		this.instanceNum = params.instanceNum;
-		this.categories.on('change:headingId', this.checkCategory, this);
+		this.categories.on('change:headingId', this.checkCategory, this); // Changes in the model will determine the view state
 	},
 	events:{
 		'change input:checkbox':'assignCategory',
@@ -111,28 +118,30 @@ var HeadingCell = Backbone.View.extend({
 		var data = this.model.toJSON();
 		var categories = this.categories.toJSON();
 		for(var i in categories) {
-			// Pass header number to the category data so it can be used in {{#each}}
-			categories[i]['num'] = this.instanceNum;
+			// Pass header order to the category data so it can be used in {{#each}} to generate a unique checkbox id
+			categories[i]['heading_order'] = this.model.get('order');
 		}
 		$.extend(data, {categories:categories});
 		
 		this.$el.html(this.template($.extend(data)));
 		
-		// Check categories that belong to this header
+		// check category inputs for categories that belong to this header
 		var view = this;
 		this.categories.forEach(function(model){
 			view.checkCategory(model);
 		});
 		return this;
 	},
-	checkCategory:function(model){	
+	checkCategory:function(model){
 		this.$('input[id*="'+model.get('id')+'"]').prop('checked', model.get('headingId') === this.model.get('id'));
 	},
 	assignCategory: function(event){
 		var id = $(event.target).attr('id');
-		var modelId = id.substr(5); // Remove the #uid_
-		var model = this.categories.get(modelId);
+		var modelId = id.substr(5); // Remove the #uid_ prefix
+		var model = this.categories.get(modelId); // We can retrieve a model from its collection by the model's id
 			
+		// Calling set() will trigger "change:headingId" and "change" events, our HeadingCell 
+		// views are listening and will update themselves accordingly
 		if($(event.target).is(':checked')){
 			model.set('headingId', this.model.get('id'));
 		} else {
@@ -157,11 +166,11 @@ var DateRow = Backbone.View.extend({
 		this.calendarItems = params.calendarItems;
 		this.headings = params.headings;
 		this.categories = params.categories;
-	},
-	render:function(){		
+		
 		// Set the id to reflect the model
 		this.$el.attr('id', this.$el.attr('id')+this.date.toString("yyyy-MM-dd"));
-		
+	},
+	render:function(){				
 		var data = {
 			date_id:this.date.toString("yyyy-MM-dd"),
 			day:this.date.toString("ddd"),
@@ -172,12 +181,16 @@ var DateRow = Backbone.View.extend({
 		
 		var view = this;
 		this.headings.forEach(function(model){
+			// Find all the calendarItem models that belong to this cell
 			var items = new CalendarItems();
-			var headingCategories = view.categories.where({headingId:model.get('id')});
+			
+			// where() returns an array of models whose attributes match those in the passed object
+			var headingCategories = view.categories.where({headingId:model.get('id')}); 
 			for(var i in headingCategories) {
 				var categoryItems = view.calendarItems.where({categoryId:headingCategories[i].get('id'), date:view.date.toString('yyyy-MM-dd')});
 				items.add(categoryItems);
 			}
+			
 			var col = new DateCell({date:view.date, model:model, calendarItems:view.calendarItems, items:items, categories:view.categories});
 			view.$el.append(col.render().$el);
 		});
@@ -187,32 +200,37 @@ var DateRow = Backbone.View.extend({
 });
 
 var DateCell = Backbone.View.extend({
+	tagName:'td',
+	id:"date_",
+	className:"sortable",
 	template: Handlebars.compile(
-		'<td id="date_{{date_id}}" class="sortable">'
-			+'{{#each items}}'
-				+'<div id="uid_{{id}}" class="calendarItem" data-itemid="{{id}}">{{{text}}}</div>'
-			+'{{/each}}'
-		+'</td>'),
+		'{{#each items}}'
+			+'<div id="uid_{{id}}" class="calendarItem" data-itemid="{{id}}">{{{text}}}</div>'
+		+'{{/each}}'),
 	initialize:function(params){
 		this.date = params.date;
 		this.calendarItems = params.calendarItems;
 		this.items = params.items;
 		this.categories = params.categories;
+		
+		this.items.on('add remove', this.render, this); // Listen to changes in the collection and redraw. Notice that it is a space separated list
+		
+		// Set the id to reflect the model
+		this.$el.attr('id', this.$el.attr('id')+this.date.toString("yyyy-MM-dd"));
 	},
-	events:{'click':'addItem'},
-	render:function(){
-		// Illustrates using setElement
-		this.setElement(this.template({date_id:this.date.toString("yyyy-MM-dd"), items:this.items.toJSON()}));
+	events:{'click':'addItem'}, // No selector in the event key, so it is delegated to the view
+	render:function(){		
+		this.$el.html(this.template({date_id:this.date.toString("yyyy-MM-dd"), items:this.items.toJSON()}));
 		return this;
 	},
 	addItem:function(){
+		// For the sake of simplicity, we add new items to the first available category, if it exists
 		var colCategories = this.categories.where({headingId:this.model.get('id')});
 		if(colCategories.length > 0) { // Only create if there's a category to attach it to
 			var text = prompt("Text");
 			var item = new CalendarItem({date:this.date.toString("yyyy-MM-dd"), text:text, categoryId:colCategories[0].get('id')});
-			this.calendarItems.add(item);
-			this.items.add(item);
-			this.$el.replaceWith(this.template({date_id:this.date.toString("yyyy-MM-dd"), items:this.items.toJSON()}));
+			this.calendarItems.add(item); // Add to the reference collection
+			this.items.add(item); // Add to the view's renderable collection, this triggers a render
 		}
 	}
 });
@@ -312,6 +330,5 @@ var calendarItemsCollection = new CalendarItems([
 });*/
 console.log(calendarItemsCollection);
 
-
-
+// initialize the main view with the data
 (new CalendarView({headings:headingsCollection, calendarItems:calendarItemsCollection, categories:categoriesCollection})).render();
